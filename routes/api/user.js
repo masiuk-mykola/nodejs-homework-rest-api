5f1d856da88c7e0061cf76a431c43071c7e8e870
Mykola Masiuk <masiuk.mykola@gmail.com>
2022-11-15 14:59:07 +0200

    add public dir

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,12 +1,19 @@
 const express = require("express");
-const { logoutCtrl, loginCtrl, registerCtrl } = require("../../controllers/authControllers");
+const {
+  logoutCtrl,
+  loginCtrl,
+  registerCtrl,
+} = require("../../controllers/authControllers");
 const {
   currentUserCtrl,
   userStatusCtrl,
 } = require("../../controllers/userController");
 
 const { checkToken } = require("./middleware/checkToken");
-const { registertSchema, loginSchema } = require("./middleware/schemes/userValidSchema");
+const {
+  registertSchema,
+  loginSchema,
+} = require("./middleware/schemes/userValidSchema");
 const { validation } = require("./middleware/validationBody");
 
 const router = express.Router();
